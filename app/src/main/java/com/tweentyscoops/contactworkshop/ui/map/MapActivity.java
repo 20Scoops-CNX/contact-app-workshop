6a2e23e002a8a2937d2e9bcc7565247594d4a623
Jedsda Tiwongvorakul <jt@20scoops.net>
2018-03-09 21:42:34 +0700

    fix build not yet

--- app/src/main/java/com/tweentyscoops/contactworkshop/ui/map/MapActivity.java
+++ app/src/main/java/com/tweentyscoops/contactworkshop/ui/map/MapActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,39 +17,38 @@ import com.google.android.gms.maps.model.LatLng;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.tweentyscoops.contactworkshop.R;
+import com.tweentyscoops.contactworkshop.ui.form.FormContactActivity;
 
 import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
-    private Button btnDone, btnBack;
+    private AppCompatButton btnDone;
     public static final String KEY_LOCATION_LAT = "map_lat";
     public static final String KEY_LOCATION_LNG = "map_lng";
+    private SupportMapFragment mapFragment;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkPermissionLocation();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Dexter.withActivity(this)
-                .withPermissions(
-                        Manifest.permission.INTERNET,
-                        Manifest.permission.ACCESS_FINE_LOCATION)
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
-                }).check();
         setContentView(R.layout.activity_map);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
         btnDone = findViewById(R.id.btnDone);
-        btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnBack).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
@@ -63,11 +65,34 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.putExtra(KEY_LOCATION_LAT, mMap.getCameraPosition().target.latitude + "");
-                intent.putExtra(KEY_LOCATION_LNG, mMap.getCameraPosition().target.longitude + "");
+                intent.putExtra(KEY_LOCATION_LAT, String.valueOf(mMap.getCameraPosition().target.latitude));
+                intent.putExtra(KEY_LOCATION_LNG, String.valueOf(mMap.getCameraPosition().target.longitude));
                 setResult(RESULT_OK, intent);
                 finish();
             }
         });
+    }
+
+    private void checkPermissionLocation() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        if (mapFragment != null) {
+                            mapFragment.getMapAsync(MapActivity.this);
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MapActivity.this, R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
     }
 }
--- app/src/main/java/com/tweentyscoops/contactworkshop/ui/webview/WebViewActivity.java
+++ app/src/main/java/com/tweentyscoops/contactworkshop/ui/webview/WebViewActivity.java
@@ -1,28 +1,28 @@
 package com.tweentyscoops.contactworkshop.ui.webview;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+
 import com.tweentyscoops.contactworkshop.R;
 
+@SuppressLint("Registered")
 public class WebViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-        setWebView();
         setupView();
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private void setupView() {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.detail);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    private void setWebView() {
-        WebView WebViw = (WebView) findViewById(R.id.webView);
+        WebView WebViw = findViewById(R.id.webView);
         WebViw.getSettings().setJavaScriptEnabled(true);
         WebViw.loadUrl("https://www.google.co.th/");
     }
